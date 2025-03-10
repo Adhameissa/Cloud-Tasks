395fe401bf97abfe718ad349aa1f6a3cbf1961bf
Adhameissa <125902941+Adhameissa@users.noreply.github.com>
2025-03-10 21:25:47 +0200

    Delete test directory

--- test/models/Category.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const categorySchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    subcategories: [{ type: mongoose.Schema.Types.ObjectId, ref: "Subcategory" }]
-});
-
-module.exports = mongoose.model("Category", categorySchema);
--- test/models/Subcategory.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const subcategorySchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" }
-});
-
-module.exports = mongoose.model("Subcategory", subcategorySchema);
